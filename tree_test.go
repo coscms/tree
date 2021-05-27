a6a60a94cd0fb70d5bbec5b9caecb14ec534089e
Hank Shen <swhbox@foxmail.com>
2021-05-27 23:48:04 +0800

    update

--- tree_test.go
+++ tree_test.go
@@ -20,6 +20,7 @@ func (t *TestItem) GetParentID() int64 {
 }
 
 func TestTree(t *testing.T) {
+	// 6 -> 4 -> 2
 	list := []Row{
 		&TestItem{ID: 1, Text: `one`, ParentID: 0},
 		&TestItem{ID: 2, Text: `two`, ParentID: 4},
@@ -40,4 +41,40 @@ func TestTree(t *testing.T) {
 	if (*(*(*data)[3].Children)[0].Children)[0].Row.(*TestItem).ID != 2 {
 		t.Fatalf(`(*(*(*data)[3].Children)[0].Children)[0].Row.(*TestItem).ID == %v, expected 2`, (*(*(*data)[3].Children)[0].Children)[0].Row.(*TestItem).ID)
 	}
+}
+
+func TestTreeEndlessLoop(t *testing.T) {
+	// 2 -> 6 -> 4 -> 2
+	// 5 -> 5
+	list := []Row{
+		&TestItem{ID: 1, Text: `one`, ParentID: 0},
+		&TestItem{ID: 2, Text: `two`, ParentID: 4},
+		&TestItem{ID: 3, Text: `three`, ParentID: 2},
+		&TestItem{ID: 4, Text: `four`, ParentID: 6},
+		&TestItem{ID: 5, Text: `five`, ParentID: 5},
+		&TestItem{ID: 6, Text: `fix`, ParentID: 3},
+		&TestItem{ID: 7, Text: `seven`, ParentID: 0},
+	}
+	data, _ := Build(list)
+	Dump(data)
+}
+
+func TestTreeEndlessLoop2(t *testing.T) {
+	// 2 -> 6 -> 2
+	list := []Row{
+		&TestItem{ID: 1, Text: `one`, ParentID: 0},
+		&TestItem{ID: 2, Text: `two`, ParentID: 6},
+		&TestItem{ID: 6, Text: `fix`, ParentID: 2},
+	}
+	data, _ := Build(list)
+	Dump(data)
+	//panic(``)
+	//因为是通过自顶而下的方向构建树，死循环的结构没有顶部入口，故而会被忽略掉
+	//                              (1)     (2)     (3)
+	//                              / \      |       |
+	//                           (10) (11)  (12)    (13)
+	//                            /     \
+	//                          (21)    (22)
+	//                                    \
+	//                                    (32)
 }
